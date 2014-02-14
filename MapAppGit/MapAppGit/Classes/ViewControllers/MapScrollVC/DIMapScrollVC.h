51672150b4723c9a9430326d960d888e80f97e1c
Dmitry Ivanov <topolog@icloud.com>
2014-02-14 11:55:07 +0400

    trying to do..

---
+++ MapAppGit/MapAppGit/Classes/ViewControllers/MapScrollVC/DIMapScrollVC.h
@@ -0,0 +1,13 @@
+//
+//  DIMapScrollVC.h
+//  MapAppGit
+//
+//  Created by Dmitry Ivanov on 08.02.14.
+//  Copyright (c) 2014 Dmitry Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DIMapScrollVC : UIViewController
+
+@end
---
+++ MapAppGit/MapAppGit/Classes/ViewControllers/MapScrollVC/DIMapScrollVC.m
@@ -0,0 +1,38 @@
+//
+//  DIMapScrollVC.m
+//  MapAppGit
+//
+//  Created by Dmitry Ivanov on 08.02.14.
+//  Copyright (c) 2014 Dmitry Ivanov. All rights reserved.
+//
+
+#import "DIMapScrollVC.h"
+
+@interface DIMapScrollVC ()
+
+@end
+
+@implementation DIMapScrollVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
