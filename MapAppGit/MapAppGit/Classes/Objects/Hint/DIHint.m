970336b9d253abdaccf84bf22f6eeefc3d7c3c2e
Dmitry Ivanov <topolog@icloud.com>
2014-04-24 21:04:38 +0400

    + Hints + LocalNotification + Location working in bg

---
+++ MapAppGit/MapAppGit/Classes/Objects/Hint/DIHint.m
@@ -0,0 +1,13 @@
+//
+//  DIHint.m
+//  MapAppGit
+//
+//  Created by Dmitry Ivanov on 24.04.14.
+//  Copyright (c) 2014 Dmitry Ivanov. All rights reserved.
+//
+
+#import "DIHint.h"
+
+@implementation DIHint
+
+@end
